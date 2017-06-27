edb64db0c433a970e0302a7fa51ca6ae8c166ca9
avichai <avichai.bendavid@mail.huji.ac.il>
2017-06-27 14:09:15 +0300

    Initial commit

---
+++ ex4.py
@@ -0,0 +1,143 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def create_train_val_test(num, data):
+    data = data.reshape(-1, 1)
+    x = np.repeat(data, num, axis=1)
+    x **= np.arange(num)
+    return x.transpose()
+
+
+def train_model(all_w, loss_on_valid, loss_on_train, num, y_train, y_valid,
+                x_train, x_valid):
+    x = create_train_val_test(num+1, x_train)
+    x_x_t = np.dot(x, x.transpose())
+    x_x_t_inv = np.linalg.pinv(x_x_t)
+    x_y = np.dot(x, y_train)
+    w_star = np.dot(x_x_t_inv, x_y)
+    all_w.append(w_star)
+
+    x_t = x.transpose()
+    result = np.dot(x_t, w_star)
+    loss = np.sum(np.square(result - y_train), 0) / y_train.shape[0]
+    loss_on_train.append(loss)
+
+    x_val = create_train_val_test(num+1, x_valid)
+    x_val_t = x_val.transpose()
+    result = np.dot(x_val_t, w_star)
+    loss = np.sum(np.square(result - y_valid), 0) / y_valid.shape[0]
+    loss_on_valid.append(loss)
+
+
+def run_least_square_alg(y_train, y_valid, y_test, x_train, x_valid, x_test):
+    all_w = []
+    loss_on_train = []
+    loss_on_valid = []
+    for i in range(1, 16):
+        train_model(all_w, loss_on_valid, loss_on_train, i, y_train, y_valid,
+                    x_train, x_valid)
+
+    all_w = np.array(all_w)
+    loss_on_valid = np.array(loss_on_valid)
+    loss_on_train = np.array(loss_on_train)
+    ind = np.argmin(loss_on_valid)
+    print(ind)
+    w_star = all_w[ind]
+    x_final_test = create_train_val_test(w_star.shape[0], x_test)
+    x_test_t = x_final_test.transpose()
+    result = np.dot(x_test_t, w_star)
+    loss = np.sum(np.square(result - y_test), 0) / y_test.shape[0]
+
+    print(w_star)
+    print(loss_on_valid[ind])
+    print(loss)
+
+    d = np.arange(1, 16)
+    plt.figure()
+    plt.plot(d, loss_on_valid, 'g^', d, loss_on_valid, 'g',
+             label='loss on valid')
+
+    plt.plot(d, loss_on_train, 'bs', d, loss_on_train, 'b',
+             label='loss on train')
+    plt.title('train and validate errors degree=1,...,15')
+    plt.legend(loc='upper right')
+
+    plt.figure()
+    plt.plot(d[1:], loss_on_valid[1:], 'g^', d[1:], loss_on_valid[1:], 'g',
+             label='loss on valid')
+    plt.plot(d[1:], loss_on_train[1:],
+             'bs', d[1:], loss_on_train[1:], 'b', label='loss on train')
+    plt.title('train and validate errors degree=2,...,15')
+    plt.legend(loc='upper right')
+
+    plt.show()
+
+
+def least_square(train_set, train_label_set, d):
+    x = create_train_val_test(d+1, train_set)
+    x_x_t = np.dot(x, x.transpose())
+    x_x_t_inv = np.linalg.pinv(x_x_t)
+    x_y = np.dot(x, train_label_set)
+    w_star = np.dot(x_x_t_inv, x_y)
+    return w_star
+
+
+def get_loss(h, untrained_set, untrained_set_label):
+    x = create_train_val_test(h.shape[0], untrained_set)
+    x_t = x.transpose()
+    result = np.dot(x_t, h)
+    loss = np.sum(np.square(result - untrained_set_label), 0) / untrained_set_label.shape[0]
+    return loss
+
+
+def run_k_fold_alg(x_fold, y_fold):
+    x_split = np.split(x_fold, 5)
+    y_split = np.split(y_fold, 5)
+
+    error = []
+
+    for i in range(1, 16):
+        loss_h = []
+        for j in range(5):
+            x = np.concatenate([x_split[k] for k in range(5) if k != j])
+            y = np.concatenate([y_split[k] for k in range(5) if k != j])
+
+            h = least_square(x, y, i)
+            loss = get_loss(h, x_split[j], y_split[j])
+            loss_h.append(loss)
+        loss_h = np.array(loss_h)
+        error.append(np.sum(loss_h)/5)
+
+    error = np.array(error)
+    # print(error)
+    ind = np.argmin(error)
+    print(ind)
+
+    h_star = least_square(x_fold, y_fold, ind+1)
+    print(h_star)
+
+    d = np.arange(1, 16)
+    plt.figure()
+    plt.plot(d, error, 'g^', d, error, 'g')
+    plt.title('errors degree=1,...,15')
+
+    plt.figure()
+    plt.plot(d[1:], error[1:], 'g^', d[1:], error[1:], 'g')
+    plt.title('errors degree=2,...,15')
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    y_poly = np.load("./Y_poly.npy")
+    y_train, y_valid, y_test = np.split(y_poly, 3)
+    x_poly = np.load("./X_poly.npy")
+    x_train, x_valid, x_test = np.split(x_poly, 3)
+
+    run_least_square_alg(y_train, y_valid, y_test, x_train, x_valid, x_test)
+
+    x_fold = np.concatenate((x_train, x_valid))
+    y_fold = np.concatenate((y_train, y_valid))
+
+    run_k_fold_alg(x_fold, y_fold)
---
+++ test.py
@@ -0,0 +1,108 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+POLY_DEGREES = list(range(1, 16))
+K_FOLD_CONST = 5
+
+
+def create_X_mat(X, n_params):
+    X = X.reshape(-1, 1)
+    X_mat = np.repeat(X, n_params, axis=1)
+    X_mat **= np.arange(n_params)
+    return X_mat
+
+
+def train(X, Y, d):
+    n_params = d + 1
+    X_mat = create_X_mat(X, n_params)
+    X_mat_T = X_mat.T
+
+    # least squares
+    return np.dot(np.dot(np.linalg.pinv(np.dot(X_mat_T, X_mat)), X_mat_T), Y)
+
+
+def predict(X, w):
+    n_params = w.shape[0]
+    X_mat = create_X_mat(X, n_params)
+    return np.dot(X_mat, w)
+
+
+def calc_error(Y, Y_hat):
+    return np.sum((Y - Y_hat) ** 2) / Y.shape[0]
+
+
+def kfold_val(X, Y, k):
+    X_par = np.split(X, k)
+    Y_par = np.split(Y, k)
+
+    kfold_errs = []
+    for d in POLY_DEGREES:
+        err = 0
+        for i in range(k):
+            X_no_i = np.concatenate([X_par[j] for j in range(k) if j != i])
+            Y_no_i = np.concatenate([Y_par[j] for j in range(k) if j != i])
+            w = train(X_no_i, Y_no_i, d)
+            err += calc_error(Y_par[i], predict(X_par[i], w))
+        err /= k
+        kfold_errs.append(err)
+
+    val_d = POLY_DEGREES[np.argmin(kfold_errs)]
+    val_w = train(X, Y, val_d)
+    return val_w
+
+
+def plot_errors(title, errors):
+    plt.figure()
+    plt.suptitle(title)
+    plt.subplot(121)
+    plt.title("d = 1,...,15")
+    plt.plot(POLY_DEGREES, errors)
+    plt.subplot(122)
+    plt.title("d = 2,...,15")
+    plt.plot(POLY_DEGREES[1:], errors[1:])
+
+
+def main():
+    # load data
+    X = np.load("X_poly.npy")
+    Y = np.load("y_poly.npy")
+
+    # split data
+    X_train, X_val, X_test = np.split(X, 3)
+    Y_train, Y_val, Y_test = np.split(Y, 3)
+
+    # train w_d for each d in degrees (each w_d represents the coefficients of the polynomial)
+    ws = [train(X_train, Y_train, d) for d in POLY_DEGREES]
+
+    # calc the train and validation error of each classifier
+    train_errs = [calc_error(Y_train, predict(X_train, w)) for w in ws]
+    val_errs = [calc_error(Y_val, predict(X_val, w)) for w in ws]
+
+    # find the classifier which minimizes the validation loss
+    w_star_ind = np.argmin(val_errs)
+    w_star = ws[w_star_ind]
+
+    # running k-fold to find best classifier
+    X_kfold = np.concatenate((X_train, X_val))
+    Y_kfold = np.concatenate((Y_train, Y_val))
+    w_star_kfold = kfold_val(X_kfold, Y_kfold, K_FOLD_CONST)
+
+    # print validated classifier's test error
+    w_star_test_err = calc_error(Y_test, predict(X_test, w_star))
+    w_star_kfold_test_err = calc_error(Y_test, predict(X_test, w_star_kfold))
+    print("w* =          {}".format([float("{:.6f}".format(c)) for c in w_star]))
+    print("w*_kfold =    {}".format([float("{:.6f}".format(c)) for c in w_star_kfold]))
+    print("w*-w*_kfold = {}".format([float("{:.6f}".format(c)) for c in (w_star - w_star_kfold)]))
+    print()
+    print("w* test error =       {:.6f}".format(w_star_test_err))
+    print("w*_kfold test error = {:.6f}".format(w_star_kfold_test_err))
+
+    # plotting train and validation errors
+    plot_errors("Train error", train_errs)
+    plot_errors("Validation error", val_errs)
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
